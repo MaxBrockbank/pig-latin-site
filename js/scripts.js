bad8a52bb726bbfc569b2b02802009c6ae220529
Max Brockbank <maxbrockbank1999@gmail.com>
2020-10-28 15:22:20 -0700

    add dealing with words starting with vowels or consonants
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>
    Co-authored-by: Shannon Grantski <72650879+grantskis@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,61 @@ $(document).ready(function(){
 
   $("#translate").submit( event => {
     event.preventDefault();
+    const userText = $("#userText").val();
+    const results = $("#results");
+    
+    function addWay(userText) {
+      let outputString;
+      let thisFunctionsRegExp = /[aeiou]/i;
+      if (thisFunctionsRegExp.test(userText.charAt(0))) {
+        outputString = userText + "way";
+        results.text(outputString);
+      } else {
+        // The userText MUST start with a consonant, since it failed line 11's expression checking for any vowel as first letter
+        
+        
+        // console.log("We got a false for our .test() method!");
+        
+      }
+      return outputString;
+    }
+    // addWay(userText);
+    
+    // Example: pterodactyl: Where does the first vowel occur in this word?
+    // Slice everything before this first vowel
+    // Append what just got sliced to the end?....
+    
+    // find first vowel in pterodactyl == ptE
+    // slice all letters occurring before first vowel == PT
+    // move those letters to the end of the word == erodactylPT
+    // append "ay" to the end of the new word == erodactylPTAY
+
+
+
+
+
+    function checkForDoubleStartingConsonants(word) {
+      let outputWord;
+      const regexForCons= /[bcdfghjklmnpqrstvwxyz]+/i;
+      // Check, "Does my word start with exactly 1 consonant?"
+      if (regexForCons.test(word.charAt(0))) {
+        // .match() returns an array
+        const wordStartLetters = word.match(regexForCons);
+        const wordWithoutStartLetters = word.replace(regexForCons, "");
+        console.log(wordWithoutStartLetters + wordStartLetters.join() + "ay");
+
+        // At some point we gotta get back to just a word, but do rearrangements first:
+        
+      } else {
+        // Check, "Does my word start with exactly two consonants?"
+        console.log(false);
+      }  
+    }
+    checkForDoubleStartingConsonants(userText);
+
+
+
+
   });
 
 });
